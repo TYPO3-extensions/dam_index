8ad836e3b4e2d820f4b06eee8454b3a3196e9614
Michael Stucki <michael.stucki@typo3.org>
2006-09-08 18:33:16 +0000

    1:1 update from TER
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dam_index/trunk@3685 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Provides a Media submodule for mass indexing of files.',
 	'category' => 'module',
 	'shy' => 0,
-	'version' => '1.0.2',
+	'version' => '1.0.3',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -34,6 +34,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
+			'dam' => '',
 			'php' => '4.0.0-',
 			'typo3' => '3.8.0-',
 		),
@@ -42,7 +43,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:15:{s:9:"ChangeLog";s:4:"7c40";s:21:"ext_conf_template.txt";s:4:"a5f0";s:12:"ext_icon.gif";s:4:"6103";s:14:"ext_tables.php";s:4:"b91d";s:14:"doc/manual.sxw";s:4:"ec3d";s:31:"lib/class.tx_damindex_rules.php";s:4:"94d5";s:28:"lib/locallang_indexrules.xml";s:4:"9705";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"42d3";s:14:"mod1/index.php";s:4:"47f8";s:18:"mod1/locallang.xml";s:4:"37b0";s:22:"mod1/locallang_mod.xml";s:4:"6cab";s:19:"mod1/moduleicon.gif";s:4:"4cbf";s:41:"modfunc_index/class.tx_damindex_index.php";s:4:"6df3";s:27:"modfunc_index/locallang.xml";s:4:"18d0";}',
+	'_md5_values_when_last_written' => 'a:15:{s:9:"ChangeLog";s:4:"43a0";s:21:"ext_conf_template.txt";s:4:"a5f0";s:12:"ext_icon.gif";s:4:"6103";s:14:"ext_tables.php";s:4:"b91d";s:14:"doc/manual.sxw";s:4:"ec3d";s:31:"lib/class.tx_damindex_rules.php";s:4:"94d5";s:28:"lib/locallang_indexrules.xml";s:4:"9705";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"42d3";s:14:"mod1/index.php";s:4:"47f8";s:18:"mod1/locallang.xml";s:4:"37b0";s:22:"mod1/locallang_mod.xml";s:4:"6cab";s:19:"mod1/moduleicon.gif";s:4:"4cbf";s:41:"modfunc_index/class.tx_damindex_index.php";s:4:"45d7";s:27:"modfunc_index/locallang.xml";s:4:"18d0";}',
 );
 
 ?>
